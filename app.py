5bdb725a3a69ac876a63d159725834ad91f9a3b9
sarah-unifei <146854269+sarah-unifei@users.noreply.github.com>
2023-10-23 11:53:05 +0000

    Atualizando tudo

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config('Sarah - ECOP06', 'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+st.title('Página Demo ECOP06')
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+st.dataframe(esportes)
