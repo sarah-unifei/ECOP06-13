d1376948e739ceff776cfb0e558f67c991f04400
sarah-unifei <146854269+sarah-unifei@users.noreply.github.com>
2023-10-23 12:41:21 +0000

    atualizar

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 
 st.set_page_config('Sarah - ECOP06', 'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
 st.title('Página Demo ECOP06')
-esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv', encoding = 'latin-1')
 st.dataframe(esportes)
